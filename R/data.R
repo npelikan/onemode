cca9df7b8f1e3e915c02ff2acb49f43bf63743f8
nick.pelikan <nick.pelikan@yougov.com>
2018-02-17 19:49:48 -0800

    data!

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Edge list of cosponsorships of the House of Representatives of the 110th Congress
+#'
+#' @format A dataframe with 177028 rows and 2 variables
+#' \describe{
+#'   \item{bill}{bill(arbitrary designation number)}
+#'   \item{member}{Member of Congress}
+#' }
+#' @source \url{http://jhfowler.ucsd.edu/cosponsorship.htm}
+"edge_list_110"
